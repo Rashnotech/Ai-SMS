5458d785bdd4882be2b3049214aef6b75663f34e
Rashnotech <rashnotech@gmail.com>
2024-04-07 22:10:48 +0100

    Design login and registration UI

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.index)
+    path("", views.index),
+    path("login", views.login),
+    path('register', views.register)
 ]
--- main/views.py
+++ main/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'main/index.html')
+
+def login(request):
+    return render(request, 'main/login.html')
+
+def register(request):
+    return render(request, 'main/signup.html')
